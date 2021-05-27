7c6444b3c1de1435d84004cebe828e7f3340a9ec
Kristine Legaspi <krissylegaspi@gmail.com>
2021-05-27 11:52:19 -0700

    Bool to word

--- Convert-boolean-values-to-strings-Yes-or-No.py
+++ Convert-boolean-values-to-strings-Yes-or-No.py
@@ -1,5 +1,8 @@
-# Convert boolean values to strings 'Yes' or 'No'
-# Complete the method that takes a boolean value and returns a "Yes" string for true, or a "No" string for false.
+'''
+Convert boolean values to strings 'Yes' or 'No'
+
+Complete the method that takes a boolean value and returns a "Yes" string for true, or a "No" string for false.
+'''
 
 def bool_to_word(boolean):
     if boolean == True:
